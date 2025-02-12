7ddea57915346f0d9ac59ce7e9cafe709dd8368e
deepdhar <dhar2017.slg@gmail.com>
2025-02-12 17:30:41 +0530

    reverted to localhost

--- frontend/src/screens/Register.js
+++ frontend/src/screens/Register.js
@@ -12,7 +12,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://192.168.1.8:5000/api/auth/register', {
+      const response = await axios.post('http://localhost:5000/api/auth/register', {
         username,
         password,
       });
